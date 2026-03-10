9e5fc08199dd9b8c60e2ae9ffc9f8657015c065f
yagukim77 <yagukim77@gmail.com>
2026-03-10 16:08:54 +0900

    Update ai_summary.py

--- ai_summary.py
+++ ai_summary.py
@@ -1,9 +1,12 @@
 def summarize(text):
 
+    if not isinstance(text,str):
+        return ""
+
     sentences = text.split(".")
 
     if len(sentences) > 2:
 
         return sentences[0] + "." + sentences[1]
 
-    return text
+    return text[:150]
