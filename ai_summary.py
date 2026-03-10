617a96dd427702eaa722112284e407bedca10977
yagukim77 <yagukim77@gmail.com>
2026-03-10 15:03:08 +0900

    Update ai_summary.py

--- ai_summary.py
+++ ai_summary.py
@@ -2,7 +2,8 @@ def summarize(text):
 
     sentences = text.split(".")
 
-    if len(sentences) >= 2:
+    if len(sentences) > 2:
+
         return sentences[0] + "." + sentences[1]
 
     return text
