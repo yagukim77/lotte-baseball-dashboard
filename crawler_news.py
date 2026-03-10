112a82275c65b0b45d983afc8b817a670371a88c
yagukim77 <yagukim77@gmail.com>
2026-03-10 15:02:19 +0900

    Update crawler_news.py

--- crawler_news.py
+++ crawler_news.py
@@ -3,40 +3,36 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 
-keyword = "롯데자이언츠"
+keyword = "롯데 자이언츠"
 
-google_url = f"https://news.google.com/rss/search?q={quote(keyword)}&hl=ko&gl=KR&ceid=KR:ko"
+google = f"https://news.google.com/rss/search?q={quote(keyword)}&hl=ko&gl=KR&ceid=KR:ko"
 
-naver_url = f"https://news.google.com/rss/search?q=site:naver.com+{quote(keyword)}&hl=ko&gl=KR&ceid=KR:ko"
+naver = f"https://news.google.com/rss/search?q=site:naver.com+{quote(keyword)}&hl=ko&gl=KR&ceid=KR:ko"
 
-urls = [google_url, naver_url]
+urls = [google, naver]
 
 news = []
 
 for url in urls:
 
     res = requests.get(url)
+
     soup = BeautifulSoup(res.text, "xml")
 
     items = soup.find_all("item")
 
     for i in items:
 
-        title = i.title.text
-        link = i.link.text
-        pubdate = i.pubDate.text
-
         news.append({
-            "title": title,
-            "link": link,
-            "date": pubdate
+            "title": i.title.text,
+            "link": i.link.text,
+            "date": i.pubDate.text
         })
 
 df = pd.DataFrame(news)
 
-# 중복 제거
 df = df.drop_duplicates(subset="title")
 
 df.to_csv("news.csv", index=False)
 
-print("뉴스 수집:", len(df))
+print("뉴스:", len(df))
