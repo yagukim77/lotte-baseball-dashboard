3d3a774a1250a8909391763583b99a2aac701a20
yagukim77 <yagukim77@gmail.com>
2026-03-10 16:29:16 +0900

    Update crawler_schedule.py

--- crawler_schedule.py
+++ crawler_schedule.py
@@ -2,34 +2,30 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = "https://sports.news.naver.com/kbaseball/schedule/index"
+url="https://sports.news.naver.com/kbaseball/schedule/index"
 
-headers = {"User-Agent":"Mozilla/5.0"}
+res=requests.get(url)
 
-res = requests.get(url,headers=headers)
-
-soup = BeautifulSoup(res.text,"lxml")
+soup=BeautifulSoup(res.text,"lxml")
 
 games=[]
-rows=soup.select("table tbody tr")
+
+rows=soup.select(".tb_wrap tbody tr")
 
 for r in rows:
 
-    try:
-        date=r.select_one(".td_date").text.strip()
-        teams=r.select_one(".td_vs").text.strip()
-        stadium=r.select_one(".td_stadium").text.strip()
+    cols=r.find_all("td")
 
-        games.append({
-            "date":date,
-            "match":teams,
-            "stadium":stadium
-        })
-    except:
-        pass
+    if len(cols)<3:
+        continue
 
-df=pd.DataFrame(games)
+    games.append({
+        "date":cols[0].text,
+        "match":cols[1].text,
+        "stadium":cols[2].text
+    })
 
-df.to_csv("schedule.csv",index=False)
+pd.DataFrame(games).to_csv("data/schedule.csv",index=False)
+
 
 print("경기 일정 저장 완료")
