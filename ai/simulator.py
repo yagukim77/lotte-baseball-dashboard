78b90390472b0b3bc00198f930831c2154057c0c
yagukim77 <yagukim77@gmail.com>
2026-03-16 17:14:16 +0900

    Update simulator.py

--- ai/simulator.py
+++ ai/simulator.py
@@ -1,15 +1,15 @@
 import random
 
-def simulate_game(lotte_ops, opp_ops):
+def simulate(lotte_ops, opp_ops):
 
-    base = lotte_ops / (lotte_ops + opp_ops)
+    prob = lotte_ops / (lotte_ops + opp_ops)
 
-    lotte_score = int(base * 10) + random.randint(0,2)
-    opp_score = int((1-base)*10) + random.randint(0,2)
+    lotte = int(prob * 10) + random.randint(0,2)
+    opp = int((1-prob) * 10) + random.randint(0,2)
 
-    if lotte_score > opp_score:
+    if lotte > opp:
         result = "롯데 승리"
     else:
         result = "상대팀 승리"
 
-    return lotte_score, opp_score, result
+    return lotte, opp, result
