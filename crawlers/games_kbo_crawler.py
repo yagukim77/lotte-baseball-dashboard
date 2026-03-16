cdbc4770a37bdb97b89f355dc8f70c0729e01e8c
yagukim77 <yagukim77@gmail.com>
2026-03-16 15:20:54 +0900

    Update games_kbo_crawler.py

--- crawlers/games_kbo_crawler.py
+++ crawlers/games_kbo_crawler.py
@@ -2,13 +2,12 @@ import pandas as pd
 
 games = [
 
-("2026-03-10","LG",1,0,"승"),
-("2026-03-11","LG",0,1,"패"),
-("2026-03-12","KT",1,0,"승"),
-("2026-03-13","KT",1,0,"승")
+{"date":"2026-03-10","opponent":"LG","win":5,"lose":3,"result":"W"},
+{"date":"2026-03-11","opponent":"KT","win":4,"lose":7,"result":"L"},
+{"date":"2026-03-12","opponent":"KIA","win":6,"lose":2,"result":"W"}
 
 ]
 
-df = pd.DataFrame(games,columns=["date","opponent","win","lose","result"])
+df = pd.DataFrame(games)
 
 df.to_csv("data/games.csv",index=False)
