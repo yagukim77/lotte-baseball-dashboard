9569d22900de5e96083bf0f6a7ad28d453c26aa8
yagukim77 <yagukim77@gmail.com>
2026-03-16 15:19:49 +0900

    Update players_kbo_crawler.py

--- crawlers/players_kbo_crawler.py
+++ crawlers/players_kbo_crawler.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
 players = [
-("전준우",0.812),
-("고승민",0.789),
-("윤동희",0.770),
-("레이예스",0.845),
-("유강남",0.720),
-("박승욱",0.700)
+
+{"name":"전준우","ops":0.820},
+{"name":"한동희","ops":0.780},
+{"name":"레이예스","ops":0.720},
+{"name":"유강남","ops":0.690},
+{"name":"고승민","ops":0.760}
+
 ]
 
-df = pd.DataFrame(players,columns=["name","ops"])
+df = pd.DataFrame(players)
 
 df.to_csv("data/players_stats.csv",index=False)
+
+print("선수 기록 저장")
