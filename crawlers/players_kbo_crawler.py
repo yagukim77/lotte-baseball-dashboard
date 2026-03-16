1790a057b98e1c542276f7b9c57f2673c14a7cf0
yagukim77 <yagukim77@gmail.com>
2026-03-16 17:00:21 +0900

    Update players_kbo_crawler.py

--- crawlers/players_kbo_crawler.py
+++ crawlers/players_kbo_crawler.py
@@ -1,17 +1,15 @@
 import pandas as pd
 
-players = [
+url = "https://www.koreabaseball.com/Record/Player/HitterBasic/Basic1.aspx"
 
-{"name":"전준우","ops":0.820},
-{"name":"한동희","ops":0.780},
-{"name":"레이예스","ops":0.720},
-{"name":"유강남","ops":0.690},
-{"name":"고승민","ops":0.760}
+tables = pd.read_html(url)
 
-]
+df = tables[0]
 
-df = pd.DataFrame(players)
+df = df[["선수명","OPS"]]
+
+df.columns = ["name","ops"]
 
 df.to_csv("data/players_stats.csv",index=False)
 
-print("선수 기록 저장")
+print("players stats saved")
